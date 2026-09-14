beba2f6b30e742f02ecbe18adc07b96ea0f479ed
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2026-09-14 15:09:23 -0500

    exercise 1.7

--- chapter-1/fetch/main.go
+++ chapter-1/fetch/main.go
@@ -16,14 +16,12 @@ func main() {
 			os.Exit(1)
 		}
 
-		b, err := io.ReadAll(resp.Body)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-
-		fmt.Printf("%s", b)
 	}
 }
