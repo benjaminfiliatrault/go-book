4ee74ca63be9f67402690cc6e63cd9064eab0e08
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2026-09-14 15:14:26 -0500

    exercise 1.8

--- chapter-1/fetch/main.go
+++ chapter-1/fetch/main.go
@@ -5,11 +5,19 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
-		resp, err := http.Get(url)
+
+		normalizedUrl := url
+
+		if !strings.HasPrefix(url, "http://") {
+			normalizedUrl = "https://" + url
+		}
+
+		resp, err := http.Get(normalizedUrl)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
